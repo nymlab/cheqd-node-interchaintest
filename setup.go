2de9de857a0aabd46dbdd7da337c304785c8697c
iorveth <arsen.kondratiev@protonmail.com>
2024-05-26 10:52:10 +0000

    Fix cheqd config

--- setup.go
+++ setup.go
@@ -2,11 +2,7 @@ package cheqd_interchaintest
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"testing"
-
-	"github.com/icza/dyno"
 
 	"github.com/docker/docker/client"
 	"github.com/stretchr/testify/require"
@@ -34,36 +30,23 @@ func cheqdEncoding() *testutil.TestEncodingConfig {
 const (
 	votingPeriod       = "10s"
 	maxDepositPeriod   = "10s"
+	cheqdDenom         = "ncheq"
+	neutronDenom       = "untrn"
 	haltHeightDelta    = uint64(20) // will propose upgrade this many blocks in the future
 	blocksAfterUpgrade = uint64(10)
 )
 
-func ModifyGenesisShortProposals(votingPeriod string, maxDepositPeriod string) func(ibc.ChainConfig, []byte) ([]byte, error) {
-	return func(chainConfig ibc.ChainConfig, genbz []byte) ([]byte, error) {
-		g := make(map[string]interface{})
-		if err := json.Unmarshal(genbz, &g); err != nil {
-			return nil, fmt.Errorf("failed to unmarshal genesis file: %w", err)
-		}
-		if err := dyno.Set(g, votingPeriod, "app_state", "gov", "voting_params", "voting_period"); err != nil {
-			return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
-		}
-		if err := dyno.Set(g, maxDepositPeriod, "app_state", "gov", "deposit_params", "max_deposit_period"); err != nil {
-			return nil, fmt.Errorf("failed to set max deposit period in genesis json: %w", err)
-		}
-		if err := dyno.Set(g, chainConfig.Denom, "app_state", "gov", "deposit_params", "min_deposit", 0, "denom"); err != nil {
-			return nil, fmt.Errorf("failed to set min deposit denom in genesis json: %w", err)
-		}
-		if err := dyno.Set(g, "100", "app_state", "gov", "deposit_params", "min_deposit", 0, "amount"); err != nil {
-			return nil, fmt.Errorf("failed to set min deposit amount in genesis json: %w", err)
-		}
-		out, err := json.Marshal(g)
-		if err != nil {
-			return nil, fmt.Errorf("failed to marshal genesis bytes to json: %w", err)
-		}
-		return out, nil
+func getShortProposalsGenesis(votingPeriod string, maxDepositPeriod string, denom string) []cosmos.GenesisKV {
+	return []cosmos.GenesisKV{
+		cosmos.NewGenesisKV("app_state.gov.params.voting_period", votingPeriod),
+		cosmos.NewGenesisKV("app_state.gov.params.max_deposit_period", maxDepositPeriod),
+		cosmos.NewGenesisKV("app_state.gov.params.min_deposit.0.denom", denom),
+		cosmos.NewGenesisKV("app_state.gov.params.min_deposit.0.amount", "100"),
 	}
 }
+
 func GetCheqdConfig(version string) ibc.ChainConfig {
+	ShortProposalsGenesis := getShortProposalsGenesis(votingPeriod, maxDepositPeriod, cheqdDenom)
 	return ibc.ChainConfig{
 		Type:    "cosmos",
 		Name:    "cheqd",
@@ -73,14 +56,11 @@ func GetCheqdConfig(version string) ibc.ChainConfig {
 				Repository: "ghcr.io/strangelove-ventures/heighliner/cheqd", // FOR LOCAL IMAGE USE: Docker Image Name
 				Version:    version,                                         // FOR LOCAL IMAGE USE: Docker Image Tag
 				UidGid:     "1025:1025",
-				//Repository: "ghcr.io/nymlab/cheqd-node/", // FOR LOCAL IMAGE USE: Docker Image Name
-				//Version:    version,                     // FOR LOCAL IMAGE USE: Docker Image Tag
-				//UidGid:     "1025:1025",
 			},
 		},
 		Bin:                 "cheqd-noded",
 		Bech32Prefix:        "cheqd",
-		Denom:               "ncheq",
+		Denom:               cheqdDenom,
 		CoinType:            "118",
 		GasPrices:           "50ncheq",
 		GasAdjustment:       1.3,
@@ -88,11 +68,12 @@ func GetCheqdConfig(version string) ibc.ChainConfig {
 		NoHostMount:         false,
 		ConfigFileOverrides: nil,
 		EncodingConfig:      cheqdEncoding(),
-		ModifyGenesis:       ModifyGenesisShortProposals(votingPeriod, maxDepositPeriod),
+		ModifyGenesis:       cosmos.ModifyGenesis(ShortProposalsGenesis),
 	}
 }
 
 func GetNeutronConfig() ibc.ChainConfig {
+	ShortProposalsGenesis := getShortProposalsGenesis(votingPeriod, maxDepositPeriod, neutronDenom)
 	return ibc.ChainConfig{
 		Type:    "cosmos",
 		Name:    "neutron",
@@ -106,7 +87,7 @@ func GetNeutronConfig() ibc.ChainConfig {
 		},
 		Bin:                 "neutrond",
 		Bech32Prefix:        "ntrn",
-		Denom:               "untrn",
+		Denom:               neutronDenom,
 		CoinType:            "118",
 		GasPrices:           "50untrn",
 		GasAdjustment:       1.3,
@@ -114,7 +95,7 @@ func GetNeutronConfig() ibc.ChainConfig {
 		NoHostMount:         false,
 		ConfigFileOverrides: nil,
 		EncodingConfig:      nil,
-		ModifyGenesis:       ModifyGenesisShortProposals(votingPeriod, maxDepositPeriod),
+		ModifyGenesis:       cosmos.ModifyGenesis(ShortProposalsGenesis),
 	}
 }
 
